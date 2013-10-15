9c6c9fa6baccc69d7113c4c5cad1bbf5f2835153
Tom Peerdeman <tompeerdeman93@hotmail.com>
2013-10-15 21:05:09 +0200

    Basic java servlet + MySQL

---
+++ zoekmachine/src/main/java/nl/uva/search/SearchServlet.java
@@ -0,0 +1,105 @@
+/**
+ * File: SearchServlet.java
+ * Author: Tom Peerdeman
+ */
+package nl.uva.search;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+/**
+ * @author Tom Peerdeman
+ * 
+ */
+public class SearchServlet extends HttpServlet {
+	private static final long serialVersionUID = -3460489316807949031L;
+	
+	private DataSource db;
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		
+		Context c;
+		try {
+			c = new InitialContext();
+			db = (DataSource) c.lookup("java:comp/env/jdbc/database");
+		} catch(NamingException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		Connection conn = null;
+		
+		try {
+			conn = db.getConnection();
+		} catch(SQLException e) {
+			resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			e.printStackTrace();
+			throw new ServletException(e);
+		}
+		
+		resp.getOutputStream().write(
+				"<h1>Hello world from the app!</h1>".getBytes());
+		
+		resp.getOutputStream().write(
+				conn.toString().getBytes());
+		
+		Statement stm = null;
+		ResultSet res = null;
+		try {
+			stm = conn.createStatement();
+			res =
+				stm.executeQuery("SELECT * FROM documents WHERE doc_id='ABC'");
+			while(res.next()) {
+				for(int i = 0; i < res.getMetaData().getColumnCount(); i++) {
+					resp.getOutputStream().write(
+							("<br />\n" + res.getString(i + 1)).getBytes());
+				}
+			}
+		} catch(SQLException e1) {
+			e1.printStackTrace();
+			throw new ServletException(e1);
+		} finally {
+			// Close statement
+			if(stm != null) {
+				try {
+					stm.close();
+				} catch(SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			// Close result
+			if(res != null) {
+				try {
+					res.close();
+				} catch(SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		try {
+			conn.close();
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
