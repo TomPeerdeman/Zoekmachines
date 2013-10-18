4fef44bddb405fe19cb460dfb012495926039c89
Tom Peerdeman <tompeerdeman93@hotmail.com>
2013-10-18 20:13:08 +0200

    Database with more indexes

--- zoek_db.sql
+++ zoek_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Machine: localhost
--- Genereertijd: 17 okt 2013 om 19:11
+-- Genereertijd: 18 okt 2013 om 20:11
 -- Serverversie: 5.5.32-0ubuntu0.13.04.1
 -- PHP-versie: 5.4.9-4ubuntu2.3
 
@@ -29,23 +29,24 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `documents`;
 CREATE TABLE IF NOT EXISTS `documents` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `doc_id` varchar(32) NOT NULL,
+  `doc_id` varchar(64) NOT NULL,
   `title` mediumtext,
   `contents` text,
-  `category` text NOT NULL,
-  `questions` mediumtext NOT NULL,
-  `answers` mediumtext NOT NULL,
-  `answerers` text NOT NULL,
-  `keywords` text NOT NULL,
-  `questioners` text NOT NULL,
-  `questioners_party` text NOT NULL,
-  `answerers_ministry` text NOT NULL,
-  `entering_date` date NOT NULL,
-  `answering_date` date NOT NULL,
+  `category` text,
+  `questions` mediumtext,
+  `answers` mediumtext,
+  `answerers` text,
+  `keywords` text,
+  `questioners` text,
+  `questioners_party` text,
+  `answerers_ministry` text,
+  `entering_date` date DEFAULT NULL,
+  `answering_date` date DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `doc_id` (`doc_id`),
-  FULLTEXT KEY `title` (`title`,`contents`,`category`,`questions`,`answers`,`answerers`,`answerers_ministry`,`keywords`,`questioners`,`questioners_party`,`doc_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  FULLTEXT KEY `title` (`title`,`contents`,`category`,`questions`,`answers`,`answerers`,`answerers_ministry`,`keywords`,`questioners`,`questioners_party`,`doc_id`),
+  FULLTEXT KEY `questions` (`questions`),
+  FULLTEXT KEY `answers` (`answers`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=40667 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
