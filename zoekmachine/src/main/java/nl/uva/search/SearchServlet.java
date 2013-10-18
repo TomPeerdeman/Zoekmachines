519cf7269008e54f6d957d23f73517dde8f90ac4
rubenjanssen <rubenljanssen@gmail.com>
2013-10-18 15:58:54 +0200

    implemented advanced search

--- zoekmachine/src/main/java/nl/uva/search/SearchServlet.java
+++ zoekmachine/src/main/java/nl/uva/search/SearchServlet.java
@@ -172,28 +172,7 @@ public class SearchServlet extends HttpServlet {
 			try {
 				stm = conn.createStatement();
 				res = stm.executeQuery(query);
-				// out.print("<span style='float:left;'>"
-				// +
-				// "<form method='POST' action='search'>"
-				// +
-				// "<input type='hidden' name='simple_query' />"
-				// +
-				// "<input type='text' name='query'/>"
-				// +
-				// "<input type='submit' value='Search' />"
-				// +
-				// "</form>"
-				// +
-				// "</span>"
-				// +
-				// "<span style='float:left;'>"
-				// +
-				// "<form method='POST' action='advanced'>"
-				// +
-				// "<input type='submit' name='advanced' value='Advanced Search' />"
-				// +
-				// "</form>" +
-				// "</span><br><br><br>");
+				out.print("<center><img border='0' src='elgoog.jpg' width='327' height='264'><form method='POST' action='search'><input type='hidden' name='simple_query' /><input type='text' name='query'/><input type='submit' value='Search' /></form><form method='POST' action='advanced'><input type='submit' name='advanced' value='Advanced Search' /></form></center>");
 				out.print("<table border='1'>");
 				out.print("<tr><td>#</td><td>Document Id</td><td>Title</td><td>Date of Issue</td><td>Date of Response</td><td>Issuer</td><td>Issuer's Party</td><td>Score</td></tr>");
 				int j = 0;
@@ -275,10 +254,184 @@ public class SearchServlet extends HttpServlet {
 			out.flush();
 		}
 		else if(parameters.containsKey("advanced_query")) {
+			Connection conn = null;
+			resp.setContentType("text/html");
 			PrintWriter out = resp.getWriter();
-			String query = req.getParameter("query");
-			out.print("<h1>Advanced Results:</h1>");
-			out.print(query);
+			
+			String query = "SELECT * FROM documents ";
+			int where = 0;
+			
+			if (req.getParameter("doc_id").length() != 0) {
+				query += "WHERE doc_id LIKE '%" + req.getParameter("doc_id") + "%' ";
+				where = 1;
+			}
+			if (req.getParameter("title").length() != 0) {
+				if (where == 0) {
+					query += "WHERE title LIKE '%" + req.getParameter("title") + "%' ";
+					where = 1;
+				}
+				else
+					query += "AND title LIKE '%" + req.getParameter("title") + "%' ";			
+			}
+			if (req.getParameter("category").length() != 0) {
+				if (where == 0) {
+					query += "WHERE category LIKE '%" + req.getParameter("category") + "%' ";
+					where = 1;
+				}
+				else
+					query += "AND category LIKE '%" + req.getParameter("category") + "%' ";			
+			}
+			if (req.getParameter("questions").length() != 0) {
+				if (where == 0) {
+					query += "WHERE questions LIKE '%" + req.getParameter("questions") + "%' ";
+					where = 1;
+				}
+				else
+					query += "AND questions LIKE '%" + req.getParameter("questions") + "%' ";			
+			}
+			if (req.getParameter("answers").length() != 0) {
+				if (where == 0) {
+					query += "WHERE answers LIKE '%" + req.getParameter("answers") + "%' ";
+					where = 1;
+				}
+				else
+					query += "AND answers LIKE '%" + req.getParameter("answers") + "%' ";			
+			}
+			if (req.getParameter("answerers").length() != 0) {
+				if (where == 0) {
+					query += "WHERE answerers LIKE '%" + req.getParameter("answerers") + "%' ";
+					where = 1;
+				}
+				else
+					query += "AND answerers LIKE '%" + req.getParameter("answerers") + "%' ";			
+			}
+			if (req.getParameter("keywords").length() != 0) {
+				if (where == 0) {
+					query += "WHERE keywords LIKE '%" + req.getParameter("keywords") + "%' ";
+					where = 1;
+				}
+				else
+					query += "AND keywords LIKE '%" + req.getParameter("keywords") + "%' ";			
+			}
+			if (req.getParameter("questioners").length() != 0) {
+				if (where == 0) {
+					query += "WHERE questioners LIKE '%" + req.getParameter("questioners") + "%' ";
+					where = 1;
+				}
+				else
+					query += "AND questioners LIKE '%" + req.getParameter("questioners") + "%' ";			
+			}
+			if (req.getParameter("questioners_party").length() != 0) {
+				if (where == 0) {
+					query += "WHERE questioners_party LIKE '%" + req.getParameter("questioners_party") + "%' ";
+					where = 1;
+				}
+				else
+					query += "AND questioners_party LIKE '%" + req.getParameter("questioners_party") + "%' ";			
+			}
+			if (req.getParameter("answerers_ministry").length() != 0) {
+				if (where == 0) {
+					query += "WHERE answerers_ministry LIKE '%" + req.getParameter("answerers_ministry") + "%' ";
+					where = 1;
+				}
+				else
+					query += "AND answerers_ministry LIKE '%" + req.getParameter("answerers_ministry") + "%' ";			
+			}
+			
+			try {
+				conn = db.getConnection();
+			} catch(SQLException e) {
+				resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+				e.printStackTrace();
+				throw new ServletException(e);
+			}
+			
+			Statement stm = null;
+			ResultSet res = null;
+			try {
+				stm = conn.createStatement();
+				res = stm.executeQuery(query);
+				out.print("<center><img border='0' src='elgoog.jpg' width='327' height='264'><form method='POST' action='search'><input type='hidden' name='simple_query' /><input type='text' name='query'/><input type='submit' value='Search' /></form><form method='POST' action='advanced'><input type='submit' name='advanced' value='Advanced Search' /></form></center>");
+				out.print("<table border='1'>");
+				out.print("<tr><td>#</td><td>Document Id</td><td>Title</td><td>Date of Issue</td><td>Date of Response</td><td>Issuer</td><td>Issuer's Party</td><td>Score</td></tr>");
+				int j = 0;
+				while(res.next()) {
+					j++;
+					out.print("<tr>");
+					
+					out.print("<td>");
+					out.print(j);
+					out.print("</td>");
+					
+					// Doc ID
+					out.print("<td>");
+					out.print(res.getString(2));
+					out.print("</td>");
+					
+					// Title
+					out.print("<td>");
+					out.print(res.getString(3));
+					out.print("</td>");
+					
+					// Date of issue
+					out.print("<td>");
+					out.print(res.getString(13));
+					out.print("</td>");
+					
+					// Date of response
+					out.print("<td>");
+					out.print(res.getString(14));
+					out.print("</td>");
+					
+					// Issuer
+					out.print("<td>");
+					out.print(res.getString(10));
+					out.print("</td>");
+					
+					// Issuerś Party
+					out.print("<td>");
+					out.print(res.getString(11));
+					out.print("</td>");
+					
+					// Score
+					out.print("<td>");
+					//out.print(res.getString(15));
+					out.print("</td>");
+					
+					out.print("</tr>");
+				}
+				out.print("</table>");
+			} catch(SQLException e1) {
+				e1.printStackTrace();
+				throw new ServletException(e1);
+			} finally {
+				// Close statement
+				if(stm != null) {
+					try {
+						stm.close();
+					} catch(SQLException e) {
+						e.printStackTrace();
+					}
+				}
+				
+				// Close result
+				if(res != null) {
+					try {
+						res.close();
+					} catch(SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+			
+			try {
+				conn.close();
+			} catch(SQLException e) {
+				e.printStackTrace();
+			}
+			
+			out.flush();
+			
 		}
 	}
 }
