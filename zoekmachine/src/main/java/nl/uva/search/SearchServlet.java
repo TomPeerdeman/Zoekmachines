3ceef4805c3d98f72d9ce2b357d274eba2c092b7
rubenjanssen <rubenljanssen@gmail.com>
2013-10-22 14:18:39 +0200

    fixed word cloud button

--- zoekmachine/src/main/java/nl/uva/search/SearchServlet.java
+++ zoekmachine/src/main/java/nl/uva/search/SearchServlet.java
@@ -381,10 +381,7 @@ public class SearchServlet extends HttpServlet {
 				
 				out.print("<tr>");
 				out.print("<td>");
-				out.print("<p class='"
-						+ res.getString(2)
-						+ "' style='display: none;'>This is a paragraph.</p><button class='toggle "
-						+ res.getString(2) + "'>Wordclouds</button>");
+				out.print("<a href='#' class='wordcloud'><font size='2'>Word Cloud</font></a>");
 				out.print("</td>");
 				out.print("</tr>");
 				
